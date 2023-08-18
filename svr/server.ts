838edf3cf29d29f21d88eb289502defbded0ecd9
Thomas Goossen <thomasgoossen@zeelandnet.nl>
2023-08-18 13:43:01 +0200

    Created the backend

--- svr/server.ts
+++ svr/server.ts
@@ -1,3 +1,84 @@
+import * as path from "https://deno.land/std@0.188.0/path/mod.ts";
+import {existsSync} from "https://deno.land/std/fs/mod.ts";
+
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const FILES_FOLDER = "/files";
+
+class AppDefinition {
+    name: string;
+    versions: string[];
+
+    constructor(name: string, versions: string[]) {
+        this.name = name;
+        this.versions = versions;
+    }
+}
+
+class Library {
+    apps: AppDefinition[];
+
+    constructor(apps: AppDefinition[]) {
+        this.apps = apps;
+    }
+
+    addDefinition(def: AppDefinition) {
+        this.apps.push(def);
+    }
+
+    removeDefinition(name: string) {
+        let index = -1;
+        for (let i = 0; i < this.apps.length; i++) {
+            if (this.apps[i].name == name)
+                index = i;
+        }
+
+        if (index >= 0)
+            this.apps.splice(index, 1);
+    }
+
+    async rebuild() {
+        const arr: AppDefinition[] = [];
+        let appCount = 0;
+        let vCount = 0;
+        
+        for await (const dirEntry of Deno.readDir(__dirname + FILES_FOLDER + "/")) {
+            if (dirEntry.isDirectory) {
+                const versions: string[] = []
+                appCount++;
+                
+                for await (const vEntry of Deno.readDir(__dirname + FILES_FOLDER + "/" + dirEntry.name)) {
+                    if (vEntry.name.includes(".app") && vEntry.isFile) {
+                        versions.push(vEntry.name.replace(".app", ""));
+                        vCount++;
+                    }
+                }
+
+                arr.push(new AppDefinition(dirEntry.name, versions));
+            }
+        }
+
+        console.log("Rebuilt app library with " + appCount + " apps and " + vCount + " total versions.");
+        this.apps = arr;
+    }
+}
+
+async function updateMetaData() {
+    // Create the file to write to
+    if (!existsSync(__dirname + FILES_FOLDER + "/apps.json")) {
+        const metaFile = await Deno.open(__dirname + FILES_FOLDER + "/apps.json", { createNew: true, write: true });
+        metaFile.close();
+    }
+    
+    // Rebuild the app library
+    const arr: AppDefinition[] = []
+    const lib: Library = new Library(arr);
+    await lib.rebuild();
+
+    // Write the json data to the meta file
+    const data = JSON.stringify(lib);
+    await Deno.writeTextFile(__dirname + FILES_FOLDER + "/apps.json", data);
+}
+
 async function startServer() {
     const PORT = 8765;
     const server = Deno.listen({ port: PORT });
@@ -9,9 +90,44 @@ async function startServer() {
 }
 
 async function serveHttp(conn: Deno.Conn) {
-    
+    const httpConn = Deno.serveHttp(conn);
+    for await (const requestEvent of httpConn) {
+        
+        //url is filepath
+        const url = new URL(requestEvent.request.url);
+        
+        if (url.pathname == "/") {
+            const res = new Response(Deno.readTextFileSync(__dirname + FILES_FOLDER + "/apps.json"), {
+                status: 200,
+                headers: {
+                    "content-type": "application/json; charset=utf-8"
+                }
+            });
+            await requestEvent.respondWith(res);
+        } else if (url.pathname == "/refresh") {
+            const res = new Response("Refreshing meta data", { status: 202 });
+            await requestEvent.respondWith(res);
+            updateMetaData();
+        } else {
+            const filepath = decodeURIComponent(url.pathname);
+
+            // Check if file exists and open it
+            let file;
+            try {
+                file = await Deno.open(__dirname + FILES_FOLDER + filepath, { read: true });
+            } catch {
+                const notFoundResponse = new Response("404 Not Found", { status: 404 });
+                await requestEvent.respondWith(notFoundResponse);
+                continue;
+            }
+
+            // Send file back to request
+            const readableStream = file.readable;
+            const response = new Response(readableStream);
+            await requestEvent.respondWith(response);
+        }
+    }
 }
 
-
-
+updateMetaData();
 startServer();
