9f635b607f88aacca966d3b6662ead4659e42728
Thomas Goossen <thomasgoossen@zeelandnet.nl>
2023-08-18 20:05:38 +0200

    created basic UI layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,45 @@
     import "@skeletonlabs/skeleton/themes/theme-crimson.css";
     import "@skeletonlabs/skeleton/styles/skeleton.css";
     import "../app.postcss";
+    import Icon from "@iconify/svelte";
+    import { page } from "$app/stores";
+    import {
+        AppShell,
+        AppRail,
+        AppRailTile,
+        AppRailAnchor,
+        LightSwitch,
+    } from "@skeletonlabs/skeleton";
 </script>
 
-<slot />
+<AppShell>
+    <svelte:fragment slot="sidebarLeft">
+        <AppRail>
+            <svelte:fragment slot="lead">
+                <AppRailAnchor href="/" selected={$page.url.pathname === "/"}>
+                    <center>
+                        <Icon icon="ph:gear" width="40" />
+                    </center>
+                </AppRailAnchor>
+            </svelte:fragment>
+
+            <AppRailAnchor
+                href="/about"
+                selected={$page.url.pathname === "/about"}
+            >
+                (icon)
+            </AppRailAnchor>
+
+            <svelte:fragment slot="trail">
+                <div class="card p-4">
+                    <LightSwitch />
+                </div>
+            </svelte:fragment>
+        </AppRail>
+    </svelte:fragment>
+    <center>
+        <div class="card p-4 m-10">
+            <slot />
+        </div>
+    </center>
+</AppShell>
