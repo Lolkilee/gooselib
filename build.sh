7b1d1cb0e14e81f3a2debfb1d9bf935de9c803ac
Thomas Goossen <thomasgoossen@zeelandnet.nl>
2023-08-27 11:37:07 +0200

    Changed version to v1.0.0

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ platforms=("x86_64-unknown-linux-gnu" "x86_64-pc-windows-msvc" "x86_64-apple-dar
 for plat in ${platforms[@]}; do
     deno compile -A -o ./svr/build/gl-downloader-$plat --target $plat ./svr/gl-downloader.ts
 done
+
+for plat in ${platforms[@]}; do
+    deno compile -A -o ./build/gl-packager-$plat --target $plat ./gl-packager.ts
+done
