f689e263272fd40cf74dab0d16e3114ce66a7cdb
Thomas Goossen <thomasgoossen2@gmail.com>
2023-11-20 01:37:44 +0100

    Progress on big file upload and download

--- svr/gl-packager.ts
+++ svr/gl-packager.ts
@@ -2,18 +2,27 @@
 // Input ./gl-packager [input folder] [upload password] [app name] [version] [url]
 
 import { existsSync } from "https://deno.land/std@0.198.0/fs/exists.ts";
-import { tgz } from "https://deno.land/x/compress@v0.4.4/mod.ts";
+import { tar } from "https://deno.land/x/compress@v0.4.4/mod.ts";
 
 const tmpFile = "./tmp";
+const tmpCmpFile = "./tmp.gz";
 
-export async function pack(input: string, passServer: string, appName: string, version: string, url: string) {
+async function pack(input: string, passServer: string, appName: string, version: string, url: string) {
     try {
         // Compress to tar
-        console.log("Compressing...");
-        await tgz.compress(input, tmpFile);
+        console.log("Compressing (1/2)");
+        await tar.compress(input, tmpFile);
+        console.log("Compressing (2/2)")
+        const uncFile = await Deno.open(tmpFile, { read: true });
+        const cmpFile = await Deno.open(tmpCmpFile, { create: true, write: true });
+        await uncFile.readable.pipeThrough(new CompressionStream("gzip")).pipeTo(cmpFile.writable);
+
+        // Cleanup (1)
+        if (existsSync(tmpFile))
+            await Deno.remove(tmpFile);
         
         // Send post request to server
-        const file = await Deno.open(tmpFile, { read: true });
+        const file = await Deno.open(tmpCmpFile, { read: true });
         const body = file.readable;
         console.log("Uploading...");
         const resp = await fetch(url + "/upload", {
@@ -23,12 +32,12 @@ export async function pack(input: string, passServer: string, appName: string, v
                 "app-version": version,
                 "pw": passServer
             },
-            body
+            body: body
         });
 
-        // Cleanup
-        if (existsSync(tmpFile))
-            await Deno.remove(tmpFile);
+        // Cleanup (2)
+        if (existsSync(tmpCmpFile))
+            await Deno.remove(tmpCmpFile);
         
         // Server refresh
         await fetch(url + "/refresh", {
@@ -43,7 +52,7 @@ export async function pack(input: string, passServer: string, appName: string, v
 }
 
 if (Deno.args[0] == "help" || Deno.args.length != 5) {
-    console.log("Command format: [input folder] [upload password] [app name] [version] [url] \n Example: ./gl-packager password ./SomeFolder SomeApp version1 http://localhost:8765")
+    console.log("Command format: [input folder] [upload password] [app name] [version] [url] \n Example: ./gl-packager ./SomeFolder password SomeApp version1 http://localhost:8765")
 }
 else {
     const input = Deno.args[0];
