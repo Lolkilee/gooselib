f09e6e14957b2b483106dbc744c2a80919c436cb
Thomas Goossen <thomasgoossen@zeelandnet.nl>
2024-07-13 00:48:07 +0200

    gui login logic

---
+++ gui/src/lib/modals.ts
@@ -0,0 +1,6 @@
+import type { ModalSettings } from "@skeletonlabs/skeleton";
+
+export const loginModalSettings: ModalSettings = {
+    type: 'component',
+    component: 'loginModal'
+}
--- gui/src/routes/+layout.svelte
+++ gui/src/routes/+layout.svelte
@@ -4,34 +4,92 @@
     import { Child, Command } from '@tauri-apps/api/shell';
     import { resolveResource } from '@tauri-apps/api/path';
     import { fetch, ResponseType } from '@tauri-apps/api/http';
-    import { listen } from '@tauri-apps/api/event';
+    import {
+        getModalStore,
+        getToastStore,
+        initializeStores,
+        Modal,
+        Toast,
+        type ModalComponent,
+    } from '@skeletonlabs/skeleton';
+
+    import LoginModal from '$lib/loginModal.svelte';
+    import { loginModalSettings } from '$lib/modals';
+    import { tryHandshake } from '$lib/login';
+    import { loginSuccToast } from '$lib/toasts';
+
+    initializeStores();
 
     let clientProc: Child | null = null;
 
+    const BASE_URL = 'http://localhost:7123/';
+    const modalStore = getModalStore();
+    const toastStore = getToastStore();
+    const modalRegistry: Record<string, ModalComponent> = {
+        loginModal: { ref: LoginModal },
+    };
+
+    async function pingClient(): Promise<boolean> {
+        try {
+            const resp = await fetch(BASE_URL, {
+                method: 'GET',
+                responseType: ResponseType.Text,
+            });
+            if (resp.ok) return true;
+            else return false;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
     async function keepAliveReq() {
-        await fetch('http://localhost:7123/', {
-            method: 'GET',
-            responseType: ResponseType.Text,
-        });
+        await pingClient();
     }
 
     async function startClient() {
-        let resourcePath: string = await resolveResource('java/');
-        resourcePath = resourcePath.replace('\\\\?\\', '');
-        console.log('starting jar from: ' + resourcePath);
-        const comm = Command.sidecar('../binaries/exec_client', [resourcePath]);
-        clientProc = await comm.spawn();
+        sessionStorage.setItem('logged-in', 'false');
+        sessionStorage.setItem('login-alive', 'false');
 
-        // Test request
-        const resp = await fetch('http://localhost:7123/', {
-            method: 'GET',
-            responseType: ResponseType.Text,
-        });
-        console.log(resp);
+        const clientAlive = await pingClient();
+        if (!clientAlive) {
+            let resourcePath: string = await resolveResource('java/');
+            resourcePath = resourcePath.replace('\\\\?\\', '');
+            console.log('starting jar from: ' + resourcePath);
+            const comm = Command.sidecar('../binaries/exec_client', [
+                resourcePath,
+            ]);
+            clientProc = await comm.spawn();
+        }
+
+        await new Promise((r) => setTimeout(r, 1000));
+
+        let attempt = false;
+        const savUser = localStorage.getItem('username');
+        const savPass = localStorage.getItem('password');
+        const ip = localStorage.getItem('ip');
+
+        if (savUser && savPass && ip) {
+            attempt = await tryHandshake(savUser, savPass, ip);
+        }
+
+        if (!attempt) {
+            while (sessionStorage.getItem('logged-in') == 'false') {
+                if (sessionStorage.getItem('login-alive') == 'false') {
+                    modalStore.trigger(loginModalSettings);
+                }
+                await new Promise((r) => setTimeout(r, 100));
+            }
+        } else {
+            toastStore.trigger(loginSuccToast);
+        }
     }
 
-    setInterval(keepAliveReq, 1000);
     startClient();
+    setInterval(keepAliveReq, 1000);
 </script>
+
+<Toast />
+<Modal components={modalRegistry} />
 
 <slot />
